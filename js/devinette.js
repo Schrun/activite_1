f14d367cd83ba88e10930aebdf253522934dc716
Schrun <majordom@neuf.fr>
2017-11-07 17:46:37 +0100

    Modification des messages de suivi des tentatives

--- js/devinette.js
+++ js/devinette.js
@@ -11,7 +11,7 @@ console.log("Bienvenue dans ce jeu de devinette !");
 var solution = Math.floor(Math.random() * 100) + 1;
 
 // Décommentez temporairement cette ligne pour mieux vérifier le programme
-//console.log("(La solution est " + solution + ")");
+console.log("(La solution est " + solution + ")");
 
 // TODO : complétez le programme
 
@@ -19,6 +19,12 @@ var COUP_MAX = 6;
 var proposition;
 var numeroDeCoup = 1;
 var resultatPrecedent = "";
+
+console.log("Vous devez essayer de trouver un nombre entre 1 et 100.\n" +
+	"Vous avez " + COUP_MAX + " coups pour deviner juste.\n" +
+	"----------------------------------------------------\n" +
+	"Il vous reste " + (COUP_MAX - numeroDeCoup + 1) + " tentatives.\n" +
+	"Quelle est votre proposition (" + numeroDeCoup + " / " + COUP_MAX + ") ? ");
 
 while ((proposition != solution) && (numeroDeCoup <= COUP_MAX)) {
 	proposition = prompt ("Vous devez essayer de trouver un nombre entre 1 et 100.\n" +
@@ -30,16 +36,17 @@ while ((proposition != solution) && (numeroDeCoup <= COUP_MAX)) {
 	console.log("Proposition (" + numeroDeCoup + " / " + COUP_MAX + ") : " + proposition);
 	if (isNaN(proposition)) {
 		resultatPrecedent = "Attention ! La solution est forcément numérique ! Je ne compte pas ce coup. ";
-		console.log(resultatPrecedent);
 	}
 	else {
 		if (proposition == solution) {
-			alert("Bravo, vous avez deviné juste, c'est gagné !");
+			resultatPrecedent = "Bravo, vous avez deviné juste en " + numeroDeCoup + " coup(s), c'est gagné !";
+			alert(resultatPrecedent);
 		}
 		else {
 			if (numeroDeCoup == COUP_MAX) {
-				alert("Vous avez utilisé tous vos coups et " + proposition + " n'est pas le résultat attendu.\n" +
-					"La solution cherchée était " + solution + ", vous avez perdu !");
+				resultatPrecedent = "Vous avez utilisé tous vos coups et " + proposition + " n'est pas le résultat attendu.\n" +
+					"La solution cherchée était " + solution + ", vous avez perdu !";
+				alert(resultatPrecedent);
 			}
 			else {
 				var sens = "grande";
@@ -47,9 +54,9 @@ while ((proposition != solution) && (numeroDeCoup <= COUP_MAX)) {
 					sens = "petite";
 				}
 				resultatPrecedent = "Presque ! La solution cherchée est plus " + sens + " que " + proposition + "...\n";
-				console.log(resultatPrecedent);
 			}
 			numeroDeCoup++;
-		}	
+		}
 	}
+	console.log(resultatPrecedent);
 }
